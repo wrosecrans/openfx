b21ee4fa1d94ecc954c943c17a72722423899a36
Will Rosecrans <wrosecrans@gmail.com>
2019-09-23 22:51:35 -0700

    Add CMake for Examples to build libraries.  Doesn't yet make bundles.

---
+++ Support/ofx.cmake
@@ -0,0 +1,13 @@
+
+
+function(ofxPlugin name dir sources)
+    message("Adding plugin ${name}")
+    add_library(${name} SHARED)
+    foreach(src ${sources})
+        message("  ${dir}/${src}")
+        target_sources(${name} PRIVATE ${dir}/${src})
+    endforeach()
+
+
+    target_link_libraries(${name} OfxSupport)
+endfunction()
