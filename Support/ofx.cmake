c83597e108195d3a97646d91bf1d41a3ab57adad
wrosecrans <wrosecrans@gmail.com>
2020-09-30 19:17:49 -0700

    Initial build fixes.

--- Support/ofx.cmake
+++ Support/ofx.cmake
@@ -1,7 +1,7 @@
 
 
 function(ofxPlugin name dir sources)
-    message("Adding plugin ${name}")
+    message("Adding plugin '${name}' with ")
     add_library(${name} SHARED)
     foreach(src ${sources})
         message("  ${dir}/${src}")
